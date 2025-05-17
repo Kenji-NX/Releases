5f7534bbf061cd2c2c0dbc18cd952effe468f7c1
swurl <swurl@swurl.xyz>
2025-05-17 02:23:44 -0400

    fix windows & linux artifact names
    Signed-off-by: swurl <swurl@swurl.xyz>

--- windows/build.bat
+++ windows/build.bat
@@ -7,8 +7,6 @@ REM TODO: nightly builds
 set build_dir=bin-%arch%
 set out_dir=..\artifacts
 
-FOR /F "tokens=*" %a in ('git describe --tags --abbrev=0') do SET version=%a
-
 dotnet publish -c Release -r %target% -p:PublishSingleFile=true -p:DebugSymbols=false -o %build_dir%
 
-cp -r %build_dir% %out_dir%\Ryujinx-Windows-%version%-%arch%
+cp -r %build_dir% %out_dir%\Ryujinx-Windows-%arch%
